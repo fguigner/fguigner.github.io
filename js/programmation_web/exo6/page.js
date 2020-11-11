c6374aa41db9c2230cf113708fa61e5981b8c46d
fguigner <floran.guigner@yahoo.fr>
2020-11-11 12:02:19 +0100

    Liens TP JS + fin exo6

--- js/programmation_web/exo6/page.js
+++ js/programmation_web/exo6/page.js
@@ -3,10 +3,10 @@ let aCanvas = null;
 function init() {
     let player = document.createElement('audio');
     player.setAttribute('id','player');
-    player.setAttribute('controls','');
+    //player.setAttribute('controls','');
     player.setAttribute('preload','');
     player.setAttribute('tabindex','0');
-    player.setAttribute('ontimeupdate',"updateCanvas()");
+    player.setAttribute('ontimeupdate',"updateCanvas(this)");
 
     let source = document.createElement('source');
     source.setAttribute('type','audio/mp3');
@@ -20,7 +20,7 @@ function init() {
     bouton.setAttribute('id','bouton');
     bouton.setAttribute('type','button');
     bouton.setAttribute('value','Play');
-    //bouton.setAttribute('onclick','');
+    bouton.setAttribute('onclick','play_pause()');
 
 
     player.appendChild(source);
@@ -29,8 +29,8 @@ function init() {
 
     let canvas = document.createElement('canvas');
     canvas.setAttribute('id','myCanvas');
-    canvas.setAttribute('width',200);
-    canvas.setAttribute('height',200);
+    canvas.setAttribute('width',500);
+    canvas.setAttribute('height',500);
 
 
     let body = document.getElementById('body');
@@ -46,27 +46,38 @@ function init() {
 
 }
 
-
-function updateCanvas()
-{
-        let cercle1=new Cercle(100,100,aCanvas);
-        cercle1.ache();
+function play_pause(){
+    if (player.paused){
+        player.play();
+    }else {
+        player.pause();
+    }
 }
 
 
-let Cercle = function(x,y,canvas)
+function updateCanvas(event)
+{
+        let tps = event.currentTime;
+        let cercle=new Cercle(250,250,aCanvas, tps);
+        cercle.ache(tps);
+}
+
+
+let Cercle = function(x,y,canvas, time)
 {
     this.canvas=canvas;
     this.canvas2dContext=this.canvas.getContext("2d");
     this.x=x;
     this.y=y;
+    this.radius=time*1;
 
     this.ache = function()
     {
-
-        //this.canvas2dContext.beginPath();
-        this.canvas2dContext.arc(this.x,this.y,5,0,2*Math.PI,false);
-        this.canvas2dContext.fillStyle = "black";
+        this.canvas2dContext.clearRect(0,0,canvas.width, canvas.height);
+        this.canvas2dContext.beginPath();
+        this.canvas2dContext.arc(this.x,this.y,this.radius,0,2*Math.PI,false);
+        this.canvas2dContext.fillStyle = "grey";
+        this.canvas2dContext.fill();
         this.canvas2dContext.stroke();
     }
 }
