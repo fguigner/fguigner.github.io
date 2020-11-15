315f784f9ca81e46049f3255413f2d42b2bd426c
fguigner <floran.guigner@yahoo.fr>
2020-11-15 02:01:07 +0100

    TP 4

---
+++ js/tp2/carre.js
@@ -0,0 +1,28 @@
+function Carre(x, y, hauteur, longueur){
+    this.x=x;
+    this.y=y;
+    this.hauteur=hauteur;
+    this.longeur=longueur;
+    this.interval=null;
+}
+
+Carre.prototype.dessiner=function(){
+    console.log("dessiner ::");
+    context.fillStyle = 'red';
+    context.fillRect(this.x, this.y, this.longeur, this.hauteur);
+    console.log(this);
+
+}
+
+Carre.prototype.animer=function(that){
+    console.log("animer ::", that);
+    if (that.x < 400) {
+        context.clearRect(that.x, that.y, that.longeur, that.hauteur);
+        that.x += 10;
+        context.fillRect(that.x, that.y, that.longeur, that.hauteur);
+        //this.dessiner();
+    } else {
+        clearInterval(that.interval);
+    }
+
+}
